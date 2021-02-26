edc7372cc680e2785103d1c3b5f8df37a5f4dca5
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-02-25 23:57:09 -0500

    ZombieDice
    Trying out a module called zombiedice

--- Python/TryingOutModules/ZombieDice.py
+++ Python/TryingOutModules/ZombieDice.py
@@ -1,18 +1,25 @@
-#code from https://automatetheboringstuff.com/2e/chapter6/
+#activity from https://automatetheboringstuff.com/2e/chapter6/
 #try it out!
 #import zombiedice
+#Don't forget to do pip install zombiedice in console to run this program!
 #zombiedice.demo()
 import zombiedice
-class MyZombie:
+import random
+#bot stops after rolling two shotguns
+class StopTwoShotguns:
     def __init__(self, name):
         # All zombies must have a name:
         self.name = name
 
     def turn(self, gameState):
-        # gameState is a dict with info about the current state of the game.
-        # You can choose to ignore it in your code.
-
-        diceRollResults = zombiedice.roll() # first roll
+        shotgun = 0
+        diceRollResults = zombiedice.roll()  # first roll
+        while diceRollResults is not None:
+            shotgun += diceRollResults['shotgun']
+            if shotgun != 2:
+                diceRollResults = zombiedice.roll()
+            else:
+                break
         # roll() returns a dictionary with keys 'brains', 'shotgun', and
         # 'footsteps' with how many rolls of each type there were.
         # The 'rolls' key is a list of (color, icon) tuples with the
@@ -23,21 +30,42 @@ class MyZombie:
         #            ('green', 'shotgun')]}
 
         # REPLACE THIS ZOMBIE CODE WITH YOUR OWN:
+#bot has a 50|50 chance of stopping or continuing after the first roll
+class ContinueStop:
+    def __init__(self,name):
+        self.name = name
+    def turn(self, gameState):
+        diceRollResults = zombiedice.roll()
+        brains = 0
+        while diceRollResults is not None:
+            Active = random.randint(1,2)
+            brains += diceRollResults['brains']
+            if Active == 1:
+                diceRollResults = zombiedice.roll()
+                continue
+            elif Active == 2:
+                break
+#bot will stop after rolling two brains
+class StopTwoBrain:
+    def __init__(self,name):
+        self.name = name
+    def turn(self,gameState):
+        diceRollResults = zombiedice.roll()
         brains = 0
         while diceRollResults is not None:
             brains += diceRollResults['brains']
-
             if brains < 2:
-                diceRollResults = zombiedice.roll() # roll again
+                diceRollResults = zombiedice.roll()
             else:
                 break
-
 zombies = (
     zombiedice.examples.RandomCoinFlipZombie(name='Random'),
     zombiedice.examples.RollsUntilInTheLeadZombie(name='Until Leading'),
     zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 2 Shotguns', minShotguns=2),
     zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 1 Shotgun', minShotguns=1),
-    MyZombie(name='My Zombie Bot'),
+    StopTwoShotguns(name='StopTwoShotguns'),
+    ContinueStop(name='Continue/Stop'),
+    StopTwoBrain(name='Stop at 2 Brains')
     # Add any other zombie players here.
 )
 
