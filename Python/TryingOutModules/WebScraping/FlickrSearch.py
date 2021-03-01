43deac5b6fe47cd1c6f43c54effb1bbe92aeef9f
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-03-01 13:49:03 -0500

    MoreWebScraping

---
+++ Python/TryingOutModules/WebScraping/FlickrSearch.py
@@ -0,0 +1,8 @@
+import requests, sys, webbrowser, bs4
+print('Searching...')    # display text while downloading the search result page
+
+img = input("What would you like to search?: ")
+urlToOpen = 'https://www.flickr.com/search/?text=' + img
+print('Opening', urlToOpen)
+webbrowser.open(urlToOpen)
+#Try on cmd: Searching.py html
--- Python/TryingOutModules/WebScraping/WebScraping.py
+++
@@ -1,7 +0,0 @@
-import webbrowser, sys
-# webbrowser.open('https://inventwithpython.com/')
-
-if len(sys.argv) > 1:
-    address = ' '.join(sys.argv[1:])
-
-
