fd9aaad6af96516acd3b0a881ddaf1218560132f
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-03-01 20:44:03 -0500

    Edit
    FixedNote #

--- Python/TryingOutModules/WebScraping/FlickrSearch.py
+++ Python/TryingOutModules/WebScraping/FlickrSearch.py
@@ -1,8 +1,9 @@
 import requests, sys, webbrowser, bs4
 print('Searching...')    # display text while downloading the search result page
 
+
 img = input("What would you like to search?: ")
 urlToOpen = 'https://www.flickr.com/search/?text=' + img
 print('Opening', urlToOpen)
 webbrowser.open(urlToOpen)
-#Try on cmd: Searching.py html
+#Try on cmd: FlickrSearch.py html
